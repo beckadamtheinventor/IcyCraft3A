9952c3934edbec6ab98d2f5d5ff5f6df0d243eae
BuildTools <beckadamtheartist@gmail.com>
2020-08-07 18:26:08 -0600

    pushing to note convbin bug

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ name='IC3CRAFT.8xp'
 mkdir "bin" || echo ""
 
 echo "compiling to $name"
-fasmg src/main.asm bin/$name
+fasmg src/main.asm bin/uncompressed.8xp
+convbin -i bin/uncompressed.8xp -o bin/$name -j 8x -k 8xp-auto-decompress -n IC3CRAFT
 echo "Wrote binary to $name."
 
 read -p "Finished. Press any key to exit"
